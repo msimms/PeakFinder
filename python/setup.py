e2f4dbde06ebbaa5098e294ee43ec549ebad9fbe
Mike Simms <msimms01@gmail.com>
2021-10-11 13:00:31 -0400

    Added python setup script.

---
+++ python/setup.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+
+from setuptools import setup, find_packages
+
+__version__ = '1.0.0'
+
+setup(
+    name='PeakFinder',
+    version=__version__,
+    description='A library for identifying peaks in line data.',
+    url='https://github.com/msimms/PeakFinder',
+    packages=['peakfinder',],
+    author='Mike Simms',
+    author_email='mike@mikesimms.net',
+    license='MIT',
+    install_requires=[],
+    python_requires='>=3.5'
+)
